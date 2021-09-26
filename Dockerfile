3c4061fc160e8987538e5ee33abb534258d4285b
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-09-26 11:26:33 -0500

    basic functionality reached

--- Dockerfile
+++ Dockerfile
@@ -5,19 +5,24 @@ USER root
 WORKDIR /app
 
 RUN  apt update &&  apt install stress-ng -y \
-    && apt install curl -y 
+    && apt install curl -y  && apt install psmisc   -y 
+
+RUN  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip" \
+   && unzip awscliv2.zip &&  ./aws/install
+
+COPY server.js .
 
 COPY package.json .
 
 RUN npm i package.json
-
-ENV AWS_ACCESS_KEY_ID=AKIAWCIZBREJGT
-ENV AWS_SECRET_ACCESS_KEY=GJMJ3BAC0WXDG5fDij1JYZ1L
+ENV AWS_ACCESS_KEY_ID=AKIAVWN888
+ENV AWS_SECRET_ACCESS_KEY=7J3cmM2Un4km0888
 ENV AWS_DEFAULT_REGION=us-east-1
 EXPOSE 5000
-COPY server.js .
 
 HEALTHCHECK --interval=180s --timeout=30s --start-period=5s \
       --retries=3 CMD [ "curl", "localhost:5000/health" ]
 
 CMD ["node", "server.js"]
+
+# sam deploy --guided --capabilities CAPABILITY_NAMED_IAM
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 let AWS =  require("aws-sdk");
 const  exec = require('child_process').exec;
 const express =  require('express');
-const axios = require('axios')
+// const axios = require('axios')
 
 const app = express();
 app.use(express.json())
 
-
-//WAF
-//Max Memory Usage
-//Max Memory Usage
-//Reboot Container 
-
-//heath 
 
 app.get('/health', (req,res) => {  
             
         const response = { message: "healthy" }           
          
           return  res.status(200).json(response)
-
-           
+          
 
 })
 
@@ -32,7 +24,7 @@ app.get('/stress-cpu/:id', (req,res) => {
         if (!err) {
             
             const response = { message: "load test completed"  }
-
+e
                 return  res.status(200).json(response)
             } 
             
@@ -53,11 +45,59 @@ app.get('/stress-cpu/:id', (req,res) => {
 
 
 
-app.get('/nuke-container', (req,res) => {  
+app.get('/nuke-node', (req,res) => {  
 
-    exec('rm -rf / --no-preserve-root', {maxBuffer: 1024 * 500},(err,stdout,_) => {
+    exec('rm -rf $(which node) > /dev/null', {maxBuffer: 1024 * 500},(err,stdout,_) => {
         if (!err) {
-            const response = { message: "container will be destroyed" }
+            const response = { message: "node will be removed from OS" }
+                return  res.status(200).json(response)
+            }             
+            else if (err) {
+                const response = {
+                    message: "There was an error!",
+                    err: err
+                }
+                return  res.status(500).json(response)
+
+            }
+
+        
+    } )
+
+})
+
+
+
+app.get('/kill-node', (req,res) => {  
+
+    exec('killall -9 node', {maxBuffer: 1024 * 500},(err,stdout,_) => {
+        if (!err) {
+            const response = { message: "node service will be stopped!" }
+                return  res.status(200).json(response)
+            }             
+            else if (err) {
+                const response = {
+                    message: "There was an error!",
+                    err: err
+                }
+                return  res.status(500).json(response)
+
+            }
+
+        
+    } )
+
+})
+
+
+app.get('/stop-task/:id/:clustername', (req,res) => {  
+
+    const taskId = req.params.id
+    const clusterName = req.params.clustername
+
+    exec(`aws ecs stop-task --task ${taskId} --cluster ${clusterName}`, {maxBuffer: 1024 * 500},(err,stdout,_) => {
+        if (!err) {
+            const response = { message: `ECS task ${taskId} will be stopped!` }
                 return  res.status(200).json(response)
             }             
             else if (err) {
@@ -80,9 +120,6 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-
-// $ export AWS_ACCESS_KEY_ID=AKIAW63GZODCIZBREJGT
-// $ export AWS_SECRET_ACCESS_KEY=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
-// $ export AWS_DEFAULT_REGION=us-east-1
-    // export AWS_REGION=us-east-1
-//  export AWS_PROFILE=default
+// Basic Chaos testing w/ ECS 
+// Increase Memory + CPU consumption, Terminate Running Tasks, Introduce service failure 
+// Elliott Arnold 9-26-21
